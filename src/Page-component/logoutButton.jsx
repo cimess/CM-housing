025862fb129899985fbc81b5289bfe46fcf88c0b
ThankGod Aimuan Osagie <cimessthemanofvalor@gmail.com>
2025-09-26 19:03:11 +0100

    am fixing email handler

---
+++ src/Page-component/logoutButton.jsx
@@ -0,0 +1,30 @@
+import API from "../api/axios";
+import { useLoginAuth } from "@/Authentication/Usecontext-logic";
+import { setAccessToken } from "@/utils/authStore";
+import { useNavigate } from "react-router-dom";
+import NavLink from "@/home-component/navigation-link-component";
+export default function LogoutButton() {
+  const { setIsLogin } = useLoginAuth();
+  const navigate = useNavigate();
+
+  async function handleLogout() {
+    try {
+      await API.post("/auth/logout", {}, { withCredentials: true });
+    } catch (err) {
+      console.error("Logout error:", err);
+    } finally {
+      // clear accessToken from memory
+      setAccessToken(null);
+      setIsLogin(false);
+      navigate("/login");
+    }
+  }
+
+  return (
+    <NavLink
+      onClick={handleLogout}
+    >
+      Logout
+    </NavLink>
+  );
+}
--- src/home-component/navigation-link-component.jsx
+++ src/home-component/navigation-link-component.jsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom"
 
-export default function NavLink({text,nav,style}){
+export default function NavLink({text,nav,style,onClick}){
    return(
-      <Link className={`hover-bg text-center no-underline ${style}`} to={nav} >
+      <Link 
+      className={`hover-bg text-center no-underline ${style}`} 
+      to={nav} 
+      onClick={onClick}  >
 {text}
   </Link>
    )
