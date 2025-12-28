4f74588d1e342aeb9c9254bc2b3918465ae7e29c
ThankGod Aimuan Osagie <cimessthemanofvalor@gmail.com>
2025-12-28 09:01:59 +0100

    removed the links of fonts from the index and downloaded it internally

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './style.css'
+import "@fontsource/manrope";
+import "@fontsource/playfair-display";
 import { ThemeProvider } from './context/ThemeContext'
 import { HelmetProvider } from 'react-helmet-async';
 import { initPostHog } from './lib/posthog';
