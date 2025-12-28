36d2e16ea12beca6dd35a4ec6941cfa245240d0f
ThankGod Aimuan Osagie <cimessthemanofvalor@gmail.com>
2025-12-28 13:57:12 +0100

    fix7

--- vite.config.js
+++ vite.config.js
@@ -30,6 +30,9 @@ export default defineConfig({
             type: 'image/png'
           }
         ]
+      },
+      workbox: {
+        navigateFallbackDenylist: [/^\/sitemap\.xml$/, /^\/robots\.txt$/]
       }
     })
   ],
